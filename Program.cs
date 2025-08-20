05dca02cc2d019ba0d015820d427058f5410465c
esmangle <esmendaangelo@gmail.com>
2025-08-20 15:53:31 +0800

    update program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using PizzaManagerAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// in-memory database for testing
+builder.Services.AddDbContext<PizzaDbContext>(
+    options => options.UseInMemoryDatabase("PizzaDb")
+);
 
 var app = builder.Build();
 
