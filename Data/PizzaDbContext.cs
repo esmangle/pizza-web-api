bc7a25f6070ea949ae93fd503f61a05e03582b73
esmangle <esmendaangelo@gmail.com>
2025-08-20 16:57:11 +0800

    update dbcontext

--- Data/PizzaDbContext.cs
+++ Data/PizzaDbContext.cs
@@ -7,11 +7,23 @@ public class PizzaDbContext : DbContext
 {
 	public PizzaDbContext(DbContextOptions<PizzaDbContext> options) : base(options) { }
 
-	public DbSet<Pizza> Pizzas => Set<Pizza>();
 	public DbSet<Topping> Toppings => Set<Topping>();
+	public DbSet<Pizza> Pizzas => Set<Pizza>();
+	public DbSet<PizzaTopping> PizzaToppings => Set<PizzaTopping>();
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
-		modelBuilder.Entity<Pizza>().HasMany(p => p.Toppings).WithMany();
+		modelBuilder.Entity<PizzaTopping>(e =>
+		{
+			e.HasKey(pt => new { pt.PizzaId, pt.ToppingId });
+
+			e.HasOne(pt => pt.Pizza)
+				.WithMany(p => p.PizzaToppings)
+				.HasForeignKey(pt => pt.PizzaId);
+
+			e.HasOne(pt => pt.Topping)
+				.WithMany()
+				.HasForeignKey(pt => pt.ToppingId);
+		});
 	}
 }
